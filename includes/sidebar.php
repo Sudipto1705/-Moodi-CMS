2b359b9660d3b048fab12e6928a089e8ce90a93b
Surya Prakash Singh (Digital) <SU327185@wipro.com>
2018-08-28 02:25:49 +0530

    adding dynamic posts

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,46 +1,75 @@
 <div class="col-md-4">
 
-<!-- Blog Search Well -->
-<div class="well">
-    <h4>Blog Search</h4>
-    <div class="input-group">
-        <input type="text" class="form-control">
-        <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
-                <span class="glyphicon glyphicon-search"></span>
-        </button>
-        </span>
-    </div>
-    <!-- /.input-group -->
-</div>
+        <?php
 
-<!-- Blog Categories Well -->
-<div class="well">
-    <h4>Blog Categories</h4>
-    <div class="row">
-        <div class="col-lg-6">
-            <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-            </ul>
+        if(isset( $_POST['submit'])){
+        $search =  $_POST['search'];
+
+        $query = "SELECT * FROM posts where post_tags like '%$search%'";
+
+        $search_query = mysqli_query($connection, $query);
+
+        if(!$search_query){
+
+            die("QUERY FAILED". mysqli_error($connection));
+
+        }
+
+        $count = mysqli_num_rows($search_query);
+
+        if($count === 0){
+            echo "Sorry No Tag Matched!";
+        }else{
+
+            
+        }
+
+        }
+        ?>
+
+    <!-- Blog Search Well -->
+    <div class="well">
+        <h4>Blog Search</h4>
+        <form action="search.php" method="post">
+        <div class="input-group">
+            <input type="text" class="form-control" name="search">
+            <span class="input-group-btn">
+                <button class="btn btn-default" name="submit">
+                    <span class="glyphicon glyphicon-search"></span>
+            </button>
+            </span>
         </div>
-        <!-- /.col-lg-6 -->
-        <div class="col-lg-6">
-            <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-            </ul>
-        </div>
+        </form><!--Form End-->
+        <!-- /.input-group -->
+    </div>
+
+    <!-- Blog Categories Well -->
+    <div class="well">
+        <h4>Blog Categories</h4>
+        <div class="row">
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-lg-6 -->
+            <div class="col-lg-6">
+                <ul class="list-unstyled">
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                    <li><a href="#">Category Name</a>
+                    </li>
+                </ul>
+            </div>
         <!-- /.col-lg-6 -->
